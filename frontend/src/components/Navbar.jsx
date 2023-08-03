2945de532ec1216e54f42cc23c06e13cf0a9473c
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-08-03 16:26:18 +0200

    Navbar version mobile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className="bg-dark flex justify-between items-center h-16">
+      <Link to="/">
+        <img
+          src="../../src/assets/images/Origins Digital Logo.png"
+          className="h-4/5 mb-2"
+          alt="Origin Digital Logo"
+        />
+      </Link>
+      <Link to="/login">
+        <button
+          className="text-white mr-3 font-primary border-none"
+          type="button"
+        >
+          Log in/Sign Up
+        </button>
+      </Link>
+    </nav>
+  );
 }
