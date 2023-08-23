fc781a407607a6ed6661db4f5517e021e4bfe1d3
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-08-23 12:35:06 +0200

    hero caroussel component complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,9 @@
+import HeroCarousel from "../components/HeroCarousel";
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <div className="bg-dark">
+      <HeroCarousel />
+    </div>
+  );
 }
