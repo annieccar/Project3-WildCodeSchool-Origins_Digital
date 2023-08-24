a5ac7e206bf420863d6ba32f536546eeb75e2a6a
Thibaut Plez <thibautplez@gmail.com>
2023-08-24 14:08:16 +0200

    Added login page with login and signup functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import Router from "./routes/Router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Footbar from "./components/Footbar";
+import { CurrentUserContextProvider } from "./contexts/CurrentUserContext";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Router />
+      <CurrentUserContextProvider>
+        <Router />
+      </CurrentUserContextProvider>
       <Footbar />
       <Footer />
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -6,6 +7,7 @@ module.exports = {
       dark: "#010D18",
       orange: "#FF680A",
       white: "#FFFFFF",
+      blue: "#031E37",
     },
     extend: {
       fontFamily: {
