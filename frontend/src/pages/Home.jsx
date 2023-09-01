7ce7f318ed66e88cf41d31eed3e32a369eef42bb
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-09-01 09:54:51 +0200

    test caroussel removed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import MultipleItems from "../components/Carousel";
 import HeroCarousel from "../components/HeroCarousel";
-import TestAnnie from "../components/testAnnie";
 
 export default function Home() {
   return (
     <div className="bg-dark">
       <HeroCarousel />
-      <TestAnnie />
       <MultipleItems />
     </div>
   );
