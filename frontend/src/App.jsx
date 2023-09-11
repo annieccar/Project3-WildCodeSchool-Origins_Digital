1798ef4f1dad723b537c66023ebe8c8cc04ffe92
Thibaut Plez <thibautplez@gmail.com>
2023-09-11 15:58:02 +0200

    feat : new useContext hook : BlurredBackgroundContext to blur background elements when a modal pops up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Footbar from "./components/Footbar";
 import { CurrentUserContextProvider } from "./contexts/CurrentUserContext";
+import { BlurredBackgroundContextProvider } from "./contexts/BlurredBackgroundContext";
 
 function App() {
   return (
     <div className="h-screen bg-dark text-white">
       <Navbar />
       <CurrentUserContextProvider>
-        <Router />
+        <BlurredBackgroundContextProvider>
+          <Router />
+        </BlurredBackgroundContextProvider>
       </CurrentUserContextProvider>
       <Footbar />
       <Footer />
