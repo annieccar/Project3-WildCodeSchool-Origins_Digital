e5482283dec95df99fd165c177b666fc4a88f52c
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-09-14 18:14:47 +0200

    video duration on search result page changed

--- frontend/src/components/CategorySearchResult.jsx
+++ frontend/src/components/CategorySearchResult.jsx
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import HoverVideoPlayer from "react-hover-video-player";
 import formatStringFromDb from "../services/formatStringFromDb";
+import formatTimeFromDb from "../services/formatTimeFromDb";
 
 export default function CategorySearchResults({ categoryName, searchResults }) {
   return (
     <div>
       {searchResults?.length !== 0 && (
-        <div className="mx-2 lg:mx-5">
+        <div className="mx-2 lg:mx-10">
           <div>
             <h2 className=" text-orange text-xl drop-shadow-md font-semibold font-primary m-3 ml-12 mt-16">
               {formatStringFromDb(categoryName)}
@@ -48,10 +49,8 @@ export default function CategorySearchResults({ categoryName, searchResults }) {
                       <p className="ml-1 mt-1 font-primary font-bold text-lg">
                         {formatStringFromDb(video.name)}
                       </p>
-                      <div className="absolute px-0.5 rounded-xl top-1 right-1 z-10 drop-shadow-[0_1.2px_1.2px_rgba(100,100,100,1)]  ">
-                        <p className="text-xs text- font-medium drop-shadow-[0_0.8px_1.8px_rgba(240,100,10,1)] box-decoration-slice ">
-                          {video.duration}
-                        </p>
+                      <div className="bg-white text-black font-bold absolute rounded-lg right-2 lg:right-3 bottom-11 lg:bottom-11 text-sm px-1 font-primary z-10">
+                        {formatTimeFromDb(video.duration)}
                       </div>
                     </div>
                   </Link>
